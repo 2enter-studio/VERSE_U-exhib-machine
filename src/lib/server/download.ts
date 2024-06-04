23688149c2593faa785a034b8b08ef30166f223c
davilivob <davilivob@gmail.com>
2024-06-04 13:35:09 +0800

    downloadFile() basically done, haven't tested yet

--- src/lib/server/download.ts
+++ src/lib/server/download.ts
@@ -1,11 +1,15 @@
-import { db } from './db';
 import type { BucketName } from '@/config';
+import fs from 'fs';
 
-console.log(db);
+import { db } from './db';
 
 async function downloadFile(bucket: BucketName, name: string) {
-	const { data, error } = await db.storage.from(bucket).download(name);
+	const { data: blob, error } = await db.storage.from(bucket).download(name);
 	if (error) return { error };
+
+	const buffer = Buffer.from(await blob.arrayBuffer());
+
+	await fs.promises.writeFile(`${bucket}/${name}`, buffer);
 }
 
 export { downloadFile };
