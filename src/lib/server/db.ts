538c2c732a7cc2a333910a39c5245a66fe4949ea
davilivob <davilivob@gmail.com>
2024-06-05 21:14:28 +0800

    fix: db client not using service key

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,17 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
-import {
-	PUBLIC_LOCAL_SUPABASE_KEY,
-	PUBLIC_LOCAL_SUPABASE_URL,
-	PUBLIC_SUPABASE_KEY,
-	PUBLIC_SUPA_PROJ_ID,
-	PUBLIC_NODE_ENV
-} from '$env/static/public';
+import { ENV, LOCAL_DB_URL, LOCAL_SUPA_KEY, SUPA_KEY, SUPA_PROJ_ID } from '$env/static/private';
 
-const developing = PUBLIC_NODE_ENV !== 'production';
+// const developing = PUBLIC_NODE_ENV !== 'production';
+//
+// const url = developing ? PUBLIC_LOCAL_SUPABASE_URL : `https://${PUBLIC_SUPA_PROJ_ID}.supabase.co`;
+// const key = developing ? PUBLIC_LOCAL_SUPABASE_KEY : PUBLIC_SUPABASE_KEY;
 
-const url = developing ? PUBLIC_LOCAL_SUPABASE_URL : `https://${PUBLIC_SUPA_PROJ_ID}.supabase.co`;
-const key = developing ? PUBLIC_LOCAL_SUPABASE_KEY : PUBLIC_SUPABASE_KEY;
+// const db = createClient(url, key);
+
+const developing = ENV === 'development';
+console.log(`mode: ${developing ? 'development' : 'production'}`);
+const url = developing ? LOCAL_DB_URL : `https://${SUPA_PROJ_ID}.supabase.co`;
+const key = developing ? LOCAL_SUPA_KEY : SUPA_KEY;
 
 const db = createClient(url, key);
-
 export { db };
