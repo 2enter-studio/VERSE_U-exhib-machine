c29ec332721062dc60b5488b313962744f7974ca
davilivob <davilivob@gmail.com>
2024-06-05 21:16:07 +0800

    add backend state variable: metadata

---
+++ src/lib/server/state.ts
@@ -0,0 +1,8 @@
+import type { MetaData } from '@/config';
+
+const metadata: { old: MetaData; new: MetaData } = {
+	old: { wearings: [], meshes: [] },
+	new: { wearings: [], meshes: [] }
+};
+
+export { metadata };
