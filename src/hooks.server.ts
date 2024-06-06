f6f318fee41325e4f9281cf4f69fd28a650942a3
davilivob <davilivob@gmail.com>
2024-06-06 14:11:18 +0800

    fix: ws server not running during dev mode

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,10 @@
 import { wsHandler } from '@/server/ws';
-import type { WebSocketHandler } from 'svelte-adapter-bun';
 import type { Handle } from '@sveltejs/kit';
 
-export const handle: Handle = async ({ event, resolve }) => {
+const handle: Handle = async ({ event, resolve }) => {
 	const res = await resolve(event);
 	// console.log(event);
 	return res;
 };
 
-export const handleWebsocket: WebSocketHandler = wsHandler;
+export default { handle, wsHandler };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import 'dotenv/config'
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const { PORT } = process.env
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/ws': {
+				target: `http://127.0.0.1:${PORT}`,
+				ws: true
+			}
+		}
+	}
 });
