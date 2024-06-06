dc9c570040f0c3402229c8afe31ead4ae5f23cab
davilivob <davilivob@gmail.com>
2024-06-06 14:15:48 +0800

    optimize code: config/index.ts

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,22 +2,14 @@ import { STORAGE_BASEPATH } from '$env/static/private';
 import type { Tables } from '@/config/supabase';
 
 const BUCKET_NAMES = ['wearings', 'meshes'] as const;
+const TEXTURE_TYPES = ['baseColor', 'metallic', 'normal', 'roughness'] as const;
+
 const STORAGE_BASE = STORAGE_BASEPATH ?? 'storage';
 const METADATA_FILE = `${STORAGE_BASE}/metadata.json`;
-const TEXTURE_TYPES = ['baseColor', 'metallic', 'normal', 'roughness'];
 const EMPTY_METADATA = { wearings: [], meshes: [] };
 
 type BucketName = (typeof BUCKET_NAMES)[number];
 type MetaData = { [P in BucketName]: Tables<P>[] };
-
-const CONFIG = {
-	BUCKET_NAMES,
-	STORAGE_BASE,
-	METADATA_FILE,
-	TEXTURE_TYPES,
-	EMPTY_METADATA,
-	META_DATA_UPDATE_TIMEOUT: 3000
-} as const;
 
 type WSMessage = {
 	avatars: {
@@ -27,7 +19,13 @@ type WSMessage = {
 	}[];
 };
 
-export type { BucketName, WSMessage };
+export type { Tables, MetaData, BucketName, WSMessage };
 
-export default CONFIG;
-export type { Tables, MetaData };
+export default {
+	BUCKET_NAMES,
+	STORAGE_BASE,
+	METADATA_FILE,
+	TEXTURE_TYPES,
+	EMPTY_METADATA
+	// META_DATA_UPDATE_TIMEOUT: 3000
+} as const;
